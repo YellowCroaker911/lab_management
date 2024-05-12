99d396ec2237e38a327adcb8ca6ca49fc5530899
YellowCroaker <911145231@qq.com>
2024-05-12 21:16:43 +0800

    学期和节次的修改补了id

--- backend/src/main/java/com/backend/model/dto/session/SessionAlterDTO.java
+++ backend/src/main/java/com/backend/model/dto/session/SessionAlterDTO.java
@@ -13,11 +13,4 @@ public class SessionAlterDTO {
      */
     @NotNull(message = "节次id不能为空")
     private Long id;
-
-    /**
-     * 节次，格式为num1-num2（表示节次为num1-num2节）
-     */
-    @NotEmpty(message = "节次不能为空")
-    @Pattern(regexp = "\\d+-\\d+",message = "节次格式错误")
-    private String session;
 }
